f68da6120bad0a86afc9e6cd47e9b3a56c374947
discua <discuachinchilla05@gmail.com>
2017-03-18 07:11:27 -0600

    Se crean Las Variables

--- app/src/main/java/com/example/discua/musicabotones/MainActivity.java
+++ app/src/main/java/com/example/discua/musicabotones/MainActivity.java
@@ -1,9 +1,27 @@
 package com.example.discua.musicabotones;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer s1;
+    MediaPlayer s2;
+    MediaPlayer s3;
+    MediaPlayer s4;
+    MediaPlayer s5;
+    MediaPlayer s6;
+    MediaPlayer s7;
+    MediaPlayer s8;
+    MediaPlayer s9;
+    MediaPlayer s10;
+    MediaPlayer s11;
+    MediaPlayer s12;
+    MediaPlayer s13;
+    MediaPlayer s14;
+    MediaPlayer s15;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
